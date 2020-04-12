6c2feb7856f67ade295342e7e71dbaeefeefb344
kristofdewin <kristofdewin1982@gmail.com>
2020-04-12 18:30:53 +0200

    added basic check on addMovieClickHandler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,8 +5,11 @@ const startAddMovieButton = document.querySelector('header button');
 // const addMovieButton = document.querySelector('header').lastElementChild;
 
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 
 const backdrop = document.getElementById('backdrop');
+
+const userInputs = addMovieModal.querySelectorAll('input');
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
@@ -25,8 +28,22 @@ const backdropClickHandler = () => {
     toggleMovieModal();
 };
 
+const addMovieClickHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    //removes whitespace 
+    if(titleValue.trim()==='' || imageUrlValue.trim()==='' || ratingValue.trim()===''|| +ratingValue < 1 || +ratingValue > 5) {
+        alert('Please enter valid values (rating between 1 and 5).')
+        return;
+    }
+
+};
+
  
 
 startAddMovieButton.addEventListener('click',toggleMovieModal);
 backdrop.addEventListener('click',backdropClickHandler);
 cancelAddMovieButton.addEventListener('click',cancelAddMovieButtonClickHandler);
+confirmAddMovieButton.addEventListener('click',addMovieClickHandler);
